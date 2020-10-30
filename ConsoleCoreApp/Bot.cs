312fe06ebd7fe976fa0fc204aa777b4c44691299
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2020-10-30 22:12:59 +0500

    Change cypher task

--- ConsoleCoreApp/Bot.cs
+++ ConsoleCoreApp/Bot.cs
@@ -10,7 +10,7 @@ namespace ConsoleCoreApp
             if (questionType == "polynomial-root") return Polynomial.GetRoot(question);
             if (questionType == "determinant") return Matrix.GetAnswer(question).ToString();
             if (questionType == "moment") return MomentAnswer.GetAnswer(question);
-            if (questionType == "cypher") return Reverse.ReverseStringBuilder(question);
+            if (questionType == "cypher") return Reverse.GetAnswer(question);
             if (questionType == "statistics") return Statistics.GetAnswer(question);
             return Console.ReadLine();
         }
--- ConsoleCoreApp/Reverse.cs
+++ ConsoleCoreApp/Reverse.cs
@@ -1,15 +1,44 @@
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ConsoleCoreApp
 {
-    public static class Reverse
+    public class Reverse
     {
-        public static string ReverseStringBuilder(string str)
+        public static string GetAnswer(string data)
+        {
+            if (Regex.IsMatch(data, "[R, r]everse")) return ReverseStringBuilder(data);
+            return CaesarCode(data);
+        }
+        
+        private static string ReverseStringBuilder(string str)
         {
             StringBuilder sb = new StringBuilder();
             var text = str.Split('|');
             for (var i = text[1].Length - 1; i >= 0; i--)
                 sb.Append(text[1][i]);
+            return sb.ToString();
+        }
+
+        private static string CaesarCode(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            var parsedString = str.Split('|');
+            var data = parsedString[1];
+            var task = parsedString[0];
+            for (var i = 0; i < task.Length; i++)
+            {
+                if (char.IsDigit(task[i]))
+                    sb.Append(task[i]);
+            }
+
+            var code = int.Parse(sb.ToString()) % 26;
+            sb.Clear();
+            for (var i = 0; i < data.Length; i++)
+            {
+                sb.Append((char) (data[i] + code));
+            }
+
             return sb.ToString();
         }
     }
