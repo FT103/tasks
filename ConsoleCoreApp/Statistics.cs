b028787b70e352f05fb13f02f759b278c5b8c187
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2020-11-22 19:32:15 +0500

    Add firstMostFrequent task solution

--- ConsoleCoreApp/Statistics.cs
+++ ConsoleCoreApp/Statistics.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace ConsoleCoreApp
 {
@@ -10,16 +11,45 @@ namespace ConsoleCoreApp
             var parsedQuestion = question.Split('|');
             var fun = parsedQuestion[0];
             var stringArray = parsedQuestion[1];
-            var list = GetSortedList(stringArray);
-            if (list.Count != 0)
+            var sortedList = GetSortedList(stringArray);
+            var list = GetList(stringArray);
+            if (sortedList.Count != 0)
             {
-                if (fun == "min") return list[0].ToString();
-                if (fun == "max") return list[new Index(1, true)].ToString();
-                if (fun == "sum") return GetSum(list).ToString();
-                if (fun == "median") return GetMedian(list).ToString();
+                if (fun == "min") return sortedList[0].ToString();
+                if (fun == "max") return sortedList[new Index(1, true)].ToString();
+                if (fun == "sum") return GetSum(sortedList).ToString();
+                if (fun == "median") return GetMedian(sortedList).ToString();
+                if (fun == "firstmostfrequent") return GetFirstFrequent(list).ToString();
             }
 
             return string.Empty;
+        }
+
+        private static int GetFirstFrequent(List<int> list)
+        {
+            var values = new Dictionary<int, int>();
+            foreach (var num in list)
+            {
+                if (!values.ContainsKey(num))
+                {
+                    values.Add(num, 1);
+                }
+                
+                else values[num]++;
+            }
+
+            var mostFrequent = 0;
+            var maxFrequency = 0;
+            foreach (var key in values.Keys)
+            {
+                if (values[key] > maxFrequency)
+                {
+                    mostFrequent = key;
+                    maxFrequency = values[key];
+                }
+            }
+            
+            return mostFrequent;
         }
 
         private static int GetSum(List<int> list)
@@ -49,6 +79,18 @@ namespace ConsoleCoreApp
             }
             
             list.Sort();
+            return list;
+        }
+        
+        private static List<int> GetList(string input)
+        {
+            var arr = input.Split(' ');
+            var list = new List<int>();
+            foreach (var num in arr)
+            {
+                list.Add(int.Parse(num));
+            }
+            
             return list;
         }
     }
