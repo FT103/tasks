745f339f580fd804a00f600b5b2de8ee8710890e
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2020-11-22 11:49:30 +0500

    Bot changes

--- ConsoleCoreApp/Bot.cs
+++ ConsoleCoreApp/Bot.cs
@@ -6,7 +6,7 @@ namespace ConsoleCoreApp
     {
         public static string GetAnswer(string questionType, string question)
         {
-            if (questionType == "math") return new MathParser().GetAnswer(question);
+            if (questionType == "math") return MathTask.GetAnswer(question);
             if (questionType == "polynomial-root") return Polynomial.GetRoot(question);
             if (questionType == "determinant") return Matrix.GetAnswer(question).ToString();
             if (questionType == "moment") return MomentAnswer.GetAnswer(question);
